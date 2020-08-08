30a58bfec13ab9253bc1b282ac74fc18eeaace4a
Hansen-Nick <hansen.nick95@gmail.com>
2020-08-08 11:40:55 -0500

    8/8 extra practice problem

--- edabit12.js
+++ edabit12.js
@@ -1,3 +1,5 @@
+//You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+
 function findEvenIndex(arr) {
   let returnValue = [];
   for (let i = 0; i < arr.length; i++) {
